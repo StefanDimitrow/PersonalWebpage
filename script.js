b5b88a79b38b5cafa963d54b943257b4aa2b2166
StefanDimitrow <stefandimitrov94bg@gmail.com>
2025-03-05 19:04:21 +0200

    Added project showcase section with navigation; enhance loader and styling

---
+++ script.js
@@ -0,0 +1,47 @@
+const projects = [
+  {
+    src: "https://lotrwiki-angular.web.app/home",
+    title: "LoTR Wiki",
+    description: "A Lord of the Rings Wiki built with Angular and Firebase.",
+    github: "https://github.com/StefanDimitrow/LOTRWiki.git",
+    site: "https://lotrwiki-angular.web.app/home"
+  },
+  {
+    src: "https://hijapogled-634ba.web.app",
+    title: "Guest Hose Site",
+    description: "This is a personal project for a guest house site.",
+    github: "https://github.com/StefanDimitrow/HijaPogled.git",
+    site: "https://hijapogled-634ba.web.app"
+  }
+  // Add more projects as needed
+];
+
+let currentProjectIndex = 0;
+
+function showProject(index) {
+  const project = projects[index];
+  document.querySelector(".project-preview").src = project.src;
+  document.querySelector(".project-info h3").textContent = project.title;
+  document.querySelector(".project-info p").textContent = project.description;
+  document.querySelector(".github-link").href = project.github;
+  document.querySelector(".site-link").href = project.site;
+}
+
+function prevProject() {
+  currentProjectIndex = (currentProjectIndex - 1 + projects.length) % projects.length;
+  showProject(currentProjectIndex);
+}
+
+function nextProject() {
+  currentProjectIndex = (currentProjectIndex + 1) % projects.length;
+  showProject(currentProjectIndex);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showProject(currentProjectIndex);
+});
+
+setTimeout(() => {
+  document.querySelector(".main").classList.add("loaded");
+  document.querySelector(".main-content").classList.add("loaded");
+}, 5000);
